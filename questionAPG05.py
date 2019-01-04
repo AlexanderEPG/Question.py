8146741c7ab659ac0b7aab14f5e80d4466e78bb1
csPython <cspython@AD25TF04NGF1J-72.local>
2019-01-04 09:34:52 -0500

    added a type of point system need to find a better point system

---
+++ questionAPG05.py
@@ -0,0 +1,28 @@
+#1 question and 4 option quiz
+#by APG
+score = int(0)
+qA = int()
+q1 = False
+print("choose from the choices below by choosing the number of the option")
+print()
+print()
+print("""what is 1 + 1?
+(1)54
+(2)2
+(3)625
+(4)*click*""")
+while q1 == False:
+        try:
+                qA = int(input("Your answer >"))
+                if qA == 2:
+                        score += 1
+                        q1 = True
+                        print("correct")
+                        print("you have",score,"point(s)")
+                elif 0 < qA < 5:
+                        q1 = True
+                        print("you have", score,"point(s)")
+                else:
+                        print("Please only type positve whole integers from 1-4")
+        except ValueError:
+                print("you didnt't enter one of the numbers given, please try again")
